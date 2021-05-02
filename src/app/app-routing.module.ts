594ffb05406a7e2c92eb178fe4061e424fa7d0b9
Jose <jcalvo2095@gmail.com>
2021-05-02 11:14:03 -0500

    router protection and readme file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
   {
@@ -11,8 +13,9 @@ const routes: Routes = [
     children: [
       { path: '', component: HomeComponent }
     ],
+    canActivateChild:[AuthGuard]
   },
-  { path: 'login', component: LoginComponent},
+  { path: 'login', component: LoginComponent,canActivate:[LoginGuard] },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
