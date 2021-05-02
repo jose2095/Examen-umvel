e3aa9c8d858bf9b874218dea7703954bcfbbe7b2
Jose <jcalvo2095@gmail.com>
2021-05-02 00:42:10 -0500

    Add main menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
+  { path: 'home', component: MainComponent},
   { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,50 @@
+import { HostListener } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { AuthService } from 'src/app/services/services/auth.service';
+
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+
+
+export class MainComponent implements OnInit {
+  @ViewChild('sidenav') drawer:MatSidenav;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.screenWidth = window.innerWidth;
+    this.cdr.detectChanges();
+    
+    
+    if(this.screenWidth>1025){
+      this.drawer.mode='side';
+      this.drawer.open()
+    }else{
+      this.drawer.close()
+      this.drawer.mode='over'
+    }
+    
+  }
+
+  public screenWidth:number=0;
+
+  constructor(private cdr: ChangeDetectorRef, public _authService:AuthService) {
+        this.screenWidth = window.innerWidth;
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    if(this.screenWidth<1025) {
+      this.drawer.mode='over'; 
+      this.drawer.opened=false
+    }
+  }
+
+ 
+}
